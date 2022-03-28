b47a7e90287e66cf5ad2f5a06dfe4882a311722d
Myeonghyeon Park <mypark@c1r1s6.42seoul.kr>
2022-03-28 22:39:48 +0900

    last commit at clu

--- test/test_quote_remover.c
+++ test/test_quote_remover.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_quote_remover.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mypark <mypark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 18:23:28 by mypark            #+#    #+#             */
-/*   Updated: 2022/03/27 13:38:38 by mypark           ###   ########.fr       */
+/*   Updated: 2022/03/28 17:30:38 by mypark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,8 @@ int	main(int argc, char **argv, char **envp)
 			print_parsetree(pt_head, &cnt); // 1
 			expand_wildcard(pt_head, envp);
 			print_parsetree(pt_head, &cnt); // 2
-			remove_quote(pt_head, envp);
-			print_parsetree(pt_head, &cnt); // 3
+			// remove_quote(pt_head, envp);
+			// print_parsetree(pt_head, &cnt); // 3
 			//free_parsetree(pt_head);
 		}
 		else
