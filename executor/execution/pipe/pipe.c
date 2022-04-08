59d52d65e5aca03196008bb26cebcc83cb76ab37
Myeonghyeon Park <mypark@c1r1s5.42seoul.kr>
2022-04-08 17:23:52 +0900

    refactoring executor

---
+++ executor/execution/pipe/pipe.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 16:31:36 by mypark            #+#    #+#             */
+/*   Updated: 2022/04/08 16:36:38 by mypark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exe_tree.h"
+#include "pipe.h"
+
+int	*generate_pipe()
+{
+	int	*pipefd;
+
+	pipefd = strict_malloc(sizeof(int), 2);
+	strict_pipe(pipefd);
+	return (pipefd);
+}
+
+void	set_pipe(int **pipefd, enum e_pipe_position pos, t_exe_info *info)
+{
+	if (pos == PIPE_LAST)
+		info->pipefd_unused = -1;
+	else
+	{
+		(*pipefd) = generate_pipe();
+		info->pipefd_unused = (*pipefd)[0];
+	}
+}
---
+++ executor/execution/pipe/pipe.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 01:46:22 by mypark            #+#    #+#             */
+/*   Updated: 2022/04/08 16:30:53 by mypark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPE_H
+# define PIPE_H
+# include "info.h"
+
+enum e_pipe_position
+{
+	PIPE_LAST,
+	PIPE_NOT_LAST
+};
+
+int		*generate_pipe();
+void	set_pipe(int **pipefd, enum e_pipe_position pos, t_exe_info *info);
+
+#endif
